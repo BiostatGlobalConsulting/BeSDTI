5e770df5b2afb61e888df4c93b6cdf8d4150f418
Caitlin B. Clary <37591078+caitlinbclary@users.noreply.github.com>
2024-10-11 11:43:13 -0400

    Additional report details

--- R/besd_report.R
+++ R/besd_report.R
@@ -7,6 +7,7 @@
 #' @import dplyr
 #' @import knitr
 #' @import rmarkdown
+#' @import stringr
 #'
 #' @export
 #' @return Template BeSD survey report
@@ -36,8 +37,8 @@ besd_report <- function(
   if (analysis == "child"){
 
     report_filename <- paste0(
-      "BeSD Template Report - Child Vaccination_",
-      ANALYSIS_COUNTER, "_", Sys.Date(), ".Rmd")
+      "BeSD_Template_Report_Child_Vaccination_", ANALYSIS_COUNTER, "_",
+      stringr::str_replace_all(Sys.Date(), "-", "_"), ".Rmd")
 
     # Copy template .Rmd files
     besd_report_setup(template_name = "besd_report_subsection_plot.Rmd",
